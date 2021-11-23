f8a4fc312f8c820295f23e1d7779679d1ae06ac3
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2021-11-23 10:26:07 +0100

    added base layout for activity. Inluded zxing library to gradle

---
+++ app/src/main/java/com/example/sym/labo3/IBeaconActivity.java
@@ -0,0 +1,14 @@
+package com.example.sym.labo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IBeaconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ibeacon);
+    }
+}
--- app/src/main/java/com/example/sym/labo3/MainActivity.java
+++ app/src/main/java/com/example/sym/labo3/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.sym.labo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnQRCode;
+    private Button btnIbeacon;
+    private Button btnNfc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnQRCode = findViewById(R.id.main_btn_qr_code);
+        btnIbeacon = findViewById(R.id.main_btn_ibeacon);
+        btnNfc = findViewById(R.id.main_btn_nfc);
+
+        btnQRCode.setOnClickListener(view -> {
+            Intent intent = new Intent(this, QRCodeActivity.class);
+            startActivity(intent);
+        });
+
+        btnIbeacon.setOnClickListener(view -> {
+            Intent intent = new Intent(this, IBeaconActivity.class);
+            startActivity(intent);
+        });
+
+        btnNfc.setOnClickListener(view -> {
+            Intent intent = new Intent(this, NFCActivity.class);
+            startActivity(intent);
+        });
     }
 }
